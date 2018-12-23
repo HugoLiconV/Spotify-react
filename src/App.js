e395573191b0292d5b06a11df38a959624f50acf
Hugo <hugo_val28@hotmail.com>
2018-12-23 00:39:23 -0700

    add css in js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import SideBar from './components/SideBar';
+import PropTypes from 'prop-types';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import appRoutes from './routes/appRoutes';
+import withStyles from '@material-ui/core/styles/withStyles';
+import dashboardStyle from './assets/styles/layouts/dashboardStyle';
 
 const switchRoutes = (
   <Switch>
@@ -16,14 +19,22 @@ const switchRoutes = (
 
 class App extends Component {
   render() {
+    const { classes, ...rest } = this.props;
     return (
-      <div>
-        <h1>que onda</h1>
-        <SideBar />
-        {switchRoutes}
+      <div className={classes.wrapper}>
+        <SideBar routes={appRoutes} {...rest} />
+        <div className={classes.mainPanel}>
+          <div className={classes.content}>
+            <div className={classes.container}>{switchRoutes}</div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(dashboardStyle)(App);
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import appRoutes from '../routes/appRoutes';
 import { NavLink } from 'react-router-dom';
-import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import classNames from 'classnames';
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Hidden,
+  Drawer
+} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import SideBarStyle from '../assets/styles/components/sidebarStyle';
 
-const SideBar = () => {
-  return (
-    <List>
-      {appRoutes.map((prop, key) => {
+const SideBar = props => {
+  const { classes, routes } = props;
+  const activeRoute = routeName =>
+    props.location.pathname.indexOf(routeName) > -1 ? true : false;
+
+  const links = (
+    <List className={classes.list}>
+      {routes.map((prop, key) => {
         if (prop.redirect) return null;
+        const listItemClasses = classNames({
+          [' ' + classes.blue]: activeRoute(prop.path)
+        });
         return (
-          <NavLink to={prop.path} activeClassName="active" key={key}>
-            <ListItem button>
-              <ListItemIcon>
+          <NavLink
+            to={prop.path}
+            activeClassName="active"
+            key={key}
+            className={classes.item}
+          >
+            <ListItem button className={classes.itemLink + listItemClasses}>
+              <ListItemIcon className={classes.itemIcon}>
                 <prop.icon />
               </ListItemIcon>
-              <ListItemText>{prop.navbarName}</ListItemText>
+              <ListItemText
+                primary={prop.sidebarName}
+                className={classes.itemText}
+                disableTypography={true}
+              />
             </ListItem>
           </NavLink>
         );
       })}
     </List>
   );
+
+  return (
+    <Hidden msDown implementation="css">
+      <Drawer
+        anchor="left"
+        variant="permanent"
+        open
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div className={classes.SideBarWrapper}>{links}</div>
+      </Drawer>
+    </Hidden>
+  );
 };
 
-export default SideBar;
+SideBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  routes: PropTypes.array.isRequired
+};
+
+export default withStyles(SideBarStyle)(SideBar);
